d2ac54ed2494c8b86d74ab1f1c68fd261d1307e0
dwsantos <dwsantos1016@gmail.com>
2022-03-11 12:15:18 -0800

    Updated FizzBuzz Code

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,8 +1,38 @@
-function FizzBuzz (){
-    for (var i = 1; i <= 20; i++) {
-        var result = "";
-        if (i % 3 === 0)    { result = "Fizz"; }
-        if (i % 5 === 0)    { result += "Buzz"; }
-        if (result === "")  { result = i; }
-        console.log(result); }
+
+
+/**
+ * @param{number} n
+ * @returns{string[]}
+ */
+ function fizzbuzz(n) {
+    var values = []
+    for (let i = 1; i <= n; i++) {
+        var isMultipleOfThree = (i % 3 == 0)
+        var isMultipleOfFive = (i % 5 == 0)
+
+        if (isMultipleOfFive && isMultipleOfThree) {
+            values.push("FizzBuzz")
+        }
+        else if (isMultipleOfThree) {
+            values.push("Fizz")
+        }
+        else if (isMultipleOfFive) {
+            values.push("Buzz")
+        }
+        else {
+            values.push(i.toString())
+        }
+    }
+    return values
+}
+
+// Calculate the results of Fizz Buzz for all the numbers to 50.
+var results = fizzbuzz(50)
+
+// Print the results by appending paragraph nodes to the document body.
+for (var result of results)
+{
+    let textNode = document.createElement("p")
+    textNode.textContent = result
+    document.body.appendChild(textNode)
 }
