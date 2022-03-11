189dd3c6f18220b27c960f6762f6b9a7f09bf8ff
dwsantos <dwsantos1016@gmail.com>
2022-03-11 09:25:56 -0800

    Added Initial Structure for Fizzbuzz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,1 +1,8 @@
-
+function FizzBuzz (){
+    for (var i = 1; i <= 20; i++) {
+        var result = "";
+        if (i % 3 === 0)    { result = "Fizz"; }
+        if (i % 5 === 0)    { result += "Buzz"; }
+        if (result === "")  { result = i; }
+        console.log(result); }
+}
