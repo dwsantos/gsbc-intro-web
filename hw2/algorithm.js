0aa9b3df861416be53cf1042cb528f6b87769587
dwsantos <dwsantos1016@gmail.com>
2022-03-23 12:48:52 -0700

    HW2: Added index.html, style.css, and algorithm.js

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,1 +1,10 @@
+const numbers =[];
 
+for (let i=0; i<=100; i++){
+numbers[i]=Math.floor(Math.random() * 1000);
+}
+
+for (let i=0; i<=100; i++){
+    document.getElementById("demo").innerHTML = cars;
+<p>, </p>
+}
