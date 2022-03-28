b6ea9ea91a94e901cd0432ad46ccf7c7c93c3d91
dwsantos <dwsantos1016@gmail.com>
2022-03-28 00:44:12 -0700

    HW2: Fixed Javascript (A.1)

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -5,6 +5,6 @@ numbers[i]=Math.floor(Math.random() * 1000);
 }
 
 for (let i=0; i<=100; i++){
-    document.getElementById("demo").innerHTML = cars[i];
+    document.getElementById("demo").innerHTML = numbers[i];
 <p>, </p>
 }
