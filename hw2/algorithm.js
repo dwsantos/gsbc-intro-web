1a918f4369026ebdc0917d40cfb301fdf9ab73b9
dwsantos <dwsantos1016@gmail.com>
2022-03-31 10:49:56 -0700

    HW2: Troubleshooting JS

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -8,6 +8,7 @@ numbers[i]=Math.floor(Math.random() * 1000);
 //for (let i=0; i<=100; i++){
 //    document.getElementById("demo").innerHTML = numbers[i];
 //    target.innerHTML += "<p>, </p>";
+function num_Array(numbers){
 let nLen = numbers.length;
 
 let text = "";
@@ -20,21 +21,22 @@ for (let i = 0; i < nLen; i++) {
     text+=numbers[i] + ".";
   }
 }
- // document.getElementById("numbers").innerHTML = text;
+}
+ document.getElementById("numbers").innerHTML = text;
 
 
 //}
 
-
+function kPop(numbers){
   numbers.sort(function(a, b){return a - b});
-//  document.getElementById("largest_number").innerHTML = numbers.pop();
+document.getElementById("largest_number").innerHTML = numbers.pop();
+}
 
-
-
+function sum(numbers){
     let s=0;
     let nmLen=numbers.length;
 for (let i=0; i<nmLen; i++){
     s = s + numbers[i];
 }
-//document.getElementById("sum").innerHTML = s;
-
+document.getElementById("sum").innerHTML = s;
+}
