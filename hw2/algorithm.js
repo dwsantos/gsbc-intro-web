93301779c08252b4f16140d3ff504290bc3d7d92
dwsantos <dwsantos1016@gmail.com>
2022-04-01 08:50:09 -0700

    HW2: Formatted JS

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,41 +1,33 @@
-const numbers =[];
+const numbers = [];
 
-for (let i=0; i<=100; i++){
-numbers[i]=Math.floor(Math.random() * 1000);
+for (let i = 0; i <= 100; i++) {
+  numbers[i] = Math.floor(Math.random() * 1000);
 }
-
-
-//for (let i=0; i<=100; i++){
-//    document.getElementById("demo").innerHTML = numbers[i];
-//    target.innerHTML += "<p>, </p>";
-
+//Formats the numbers
 let nLen = numbers.length;
-
 let text = "";
+
 for (let i = 0; i < nLen; i++) {
   text += numbers[i] + ", ";
-  if(i == 100){
-    text += numbers[i] + ", and "
-    }
-    if(i ==100){
-    text+=numbers[i] + ".";
+
+    if (i == 100) {
+      text += numbers[i] + ", and ";
+      }
+    if (i == 100) {
+      text += numbers[i] + ".";
+      }
   }
-}
- document.getElementById("numbers").innerHTML = text;
+document.getElementById("numbers").innerHTML = text;
 
-
-//}
-
-  numbers.sort(function(a, b){return a - b});
+//Sorts the numbers and pops the largest one
+numbers.sort(function (a, b) {return a - b;});
 document.getElementById("largest_number").innerHTML = numbers.pop();
 
+//Adds all the numbers together
+let s = 0;
+let nmLen = numbers.length;
 
-
-
-    let s=0;
-    let nmLen=numbers.length;
-for (let i=0; i<nmLen; i++){
-    s = s + numbers[i];
-}
+for (let i = 0; i < nmLen; i++) {
+  s = s + numbers[i];
+  }
 document.getElementById("sum").innerHTML = s;
-
