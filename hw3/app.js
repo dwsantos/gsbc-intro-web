fa7bd2104dc2f087203aa433d7d9e8010af571e4
dwsantos <66379964+dwsantos@users.noreply.github.com>
2022-04-13 14:38:11 -0700

    HW3: Updated app.js

--- hw3/app.js
+++ hw3/app.js
@@ -1,10 +1,40 @@
     function towel() {
-     var answer = window.prompt("Enter your answer: ")
+     var answer = window.prompt("What gets wetter as it dries? (Warning: Text is Case Sensitive. Please Use All Lowercase Letters) Enter your answer:")
 
         if (answer == "towel" || answer == "a towel" || answer == "A Towel") {
             window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room1.html");
         }
         else{
-            alert("Wrong answer. Try again. ");
+            alert("Wrong answer. (Hint: answer is case sensitive. Please use all lowercase letters.) Try again. ");
+    }
+}
+    function incorrect() {
+     var answer = window.prompt("(Warning: Text is Case Sensitive. Please Use All Lowercase Letters) Enter your answer: ")
+
+        if (answer == "incorrect" || answer == "Incorrect" || answer == "INCORRECT") {
+            window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room4.html");
+        }
+        else{
+            alert("Wrong answer. (Hint: answer is case sensitive. Please use all lowercase letters.) Try again. ");
+    }
+}
+    function placeholder() {
+     var answer = window.prompt("(Warning: Text is Case Sensitive. Please Use All Lowercase Letters) Enter your answer: ")
+
+        if (answer == "placeholder" || answer == "a placeholder" || answer == "A Placeholder") {
+            window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room7.html");
+        }
+        else{
+            alert("Wrong answer. (Hint: answer is case sensitive. Please use all lowercase letters.) Try again. ");
+    }
+}
+    function test() {
+     var answer = window.prompt("(Warning: Text is Case Sensitive. Please Use All Lowercase Letters) Enter your answer: ")
+
+        if (answer == "test" || answer == "a test" || answer == "A Test") {
+            window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room9.html");
+        }
+        else{
+            alert("Wrong answer. (Hint: answer is case sensitive. Please use all lowercase letters.) Try again. ");
     }
 }
