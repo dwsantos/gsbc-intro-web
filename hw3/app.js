9e51fc7e5e29a7a79194c1d64cb0014121372324
dwsantos <dwsantos1016@gmail.com>
2022-04-13 12:54:29 -0700

    HW3: Added js to app and updated index

--- hw3/app.js
+++ hw3/app.js
@@ -1,1 +1,10 @@
+    function towel() {
+     var answer = window.prompt("Enter your answer: ")
 
+        if (answer == "towel" || answer == "a towel" || answer == "A Towel") {
+            window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room1.html");
+        }
+        else{
+            alert("Wrong answer. Try again. ");
+    }
+}
