9a9eab9af42111badeac06165be0c2a933eb35e7
dwsantos <66379964+dwsantos@users.noreply.github.com>
2022-04-13 14:49:12 -0700

    HW3: Added userList()

--- hw3/app.js
+++ hw3/app.js
@@ -38,3 +38,81 @@
             alert("Wrong answer. (Hint: answer is case sensitive. Please use all lowercase letters.) Try again. ");
     }
 }
+function userList(){
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "h3",
+        null,
+        "List The Answers to the Previous Riddles"
+      ),
+      React.createElement(TodoList, { items: this.state.items }),
+      React.createElement(
+        "form",
+        { onSubmit: this.handleSubmit },
+        React.createElement(
+          "label",
+          { htmlFor: "new-todo" },
+          "What needs to be done?"
+        ),
+        React.createElement("input", {
+          id: "new-todo",
+          onChange: this.handleChange,
+          value: this.state.text
+        }),
+        React.createElement(
+          "button",
+          null,
+          "Add #",
+          this.state.items.length + 1
+        )
+      )
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return React.createElement(
+      "ul",
+      null,
+      this.props.items.map(item => React.createElement(
+        "li",
+        { key: item.id },
+        item.text
+      ))
+    );
+  }
+}
+
+    root.render(React.createElement(TodoApp, null));
+}
