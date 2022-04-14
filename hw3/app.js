378ff26aa3aac52583031876434692f7b87c7127
dwsantos <66379964+dwsantos@users.noreply.github.com>
2022-04-13 19:28:46 -0700

    HW3: Added last riddles

--- hw3/app.js
+++ hw3/app.js
@@ -8,29 +8,29 @@
     }
 }
     function incorrect() {
-     var answer = window.prompt("Enter your answer: ")
+     var answer = window.prompt("What word is always spelled incorrectly in the dictionary?")
 
-        if (answer.toLowerCase() == "incorrectly") {
+        if (answer.toLowerCase() == "incorrectly" || "incorrectly ") {
             window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room3.html");
         }
         else{
             alert("Wrong answer. Try again. ");
     }
 }
-    function placeholder() {
-     var answer = window.prompt("Enter your answer: ")
+    function bank() {
+     var answer = window.prompt("I have branches, but no fruit, trunk or leaves. What am I?")
 
-        if (answer.toLowerCase() == "placeholder" || answer.toLowerCase() == "a placeholder") {
+        if (answer.toLowerCase() == "bank" || "a bank" || "bank " || "a bank ") {
             window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room6.html");
         }
         else{
             alert("Wrong answer. Try again. ");
     }
 }
-    function test() {
-     var answer = window.prompt("Enter your answer: ")
+    function river() {
+     var answer = window.prompt("What can run but never walks, has a mouth but never talks, has a head but never weeps, has a bed but never sleeps?")
 
-        if (answer.toLowerCase() == "test" || answer.toLowerCase() == "a test") {
+        if (answer.toLowerCase() == "river" || "a river" || "a river " || "river ") {
             window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/room9.html");
         }
         else{
@@ -38,9 +38,9 @@
     }
 }
     function finalQ() {
-     var answer = window.prompt("Enter your answer: ")
+     var answer = window.prompt("What has five eyes, but cannot see?")
 
-        if (answer.toLowerCase() == "jesus" || answer.toLowerCase() == "a jesus") {
+        if (answer.toLowerCase() == "the mississippi river" || "the mississippi river " || "mississippi river" || "mississippi river ") {
             window.location.replace("https://dwsantos.github.io/gsbc-intro-web/hw3/index.html");
         }
         else{
